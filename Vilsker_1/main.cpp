3d130bd8b2938eb609a4a8bfcda067c456978fce
sampletext32 <sampletext32@bk.ru>
2020-04-09 21:50:54 +0300

    Remove Function

--- Vilsker_1/main.cpp
+++ Vilsker_1/main.cpp
@@ -2,51 +2,23 @@
 
 using namespace std;
 
-double y(double x, int n)
-{
-	double temp_x = x - 1;
-	double dx = x - 1;
-	double result = 0;
-	for (int i = 1; i <= n; i++)
-	{
-		result += temp_x / i;
-		temp_x *= dx;
-	}
-	return result;
-}
-
 int main()
 {
-	int iterations = 5;
-	cout << "Iterations: " << iterations << endl;
-	for (double x = 0; x <= 1.2; x += 0.2)
+	for (int n = 5; n <= 20; n += 5)
 	{
-		double res = y(x, iterations);
-		cout << "\ty(" << x << ") = " << res << endl;
-	}
-
-	iterations = 10;
-	cout << "Iterations: " << iterations << endl;
-	for (double x = 0; x <= 1.2; x += 0.2)
-	{
-		double res = y(x, iterations);
-		cout << "\ty(" << x << ") = " << res << endl;
-	}
-
-	iterations = 15;
-	cout << "Iterations: " << iterations << endl;
-	for (double x = 0; x <= 1.2; x += 0.2)
-	{
-		double res = y(x, iterations);
-		cout << "\ty(" << x << ") = " << res << endl;
-	}
-
-	iterations = 40;
-	cout << "Iterations: " << iterations << endl;
-	for (double x = 0; x <= 1.2; x += 0.2)
-	{
-		double res = y(x, iterations);
-		cout << "\ty(" << x << ") = " << res << endl;
+		cout << "Iterations: " << n << endl;
+		for (double x = 0; x <= 1.2; x += 0.2)
+		{
+			double temp_x = x - 1;
+			double dx = x - 1;
+			double result = 0;
+			for (int i = 1; i <= n; i++)
+			{
+				result += temp_x / i;
+				temp_x *= dx;
+			}
+			cout << "\ty(" << x << ") = " << result << endl;
+		}
 	}
 	return 0;
 }
